930347a7f28517d31bb63072ae935a7e68f69218
ranggazs <74724682+ranggazs@users.noreply.github.com>
2020-11-19 23:40:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ const menu = require("./lib/menu.js");
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'ALF BOT'; 
-const instagram = 'https://instagram.com/aditiaalfians'; 
-const whatsapp = 'wa.me/6285799496179'; 
+const BotName = 'Alfa';  
+const whatsapp = 'wa.me/6285241040338'; 
 const kapanbotaktif = '24 JAM'; 
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
